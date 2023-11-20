4f774c3c21bab47e462644588e203ead34d6e802
geancampos <gean699@hotmail.com>
2023-11-20 18:39:59 -0300

    ìnício real

---
+++ JavaScript/aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,25 @@
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+// document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+
+// function tocaSomClap () {
+//     document.querySelector('#som_tecla_clap').play();
+// }
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom;
+
+
+let contador = 0
+
+while (contador < 9) {
+
+    listaDeTeclas[contador].onclick = tocaSomPom;
+    contador++
+
+    console.log(contador);
+}
