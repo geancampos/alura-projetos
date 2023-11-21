d490596f56369af58877e7b56b1a31b52a5a4b03
geancampos <gean699@hotmail.com>
2023-11-21 09:09:45 -0300

    Concluído

--- JavaScript/aluramidi-curso-arquivos-iniciais/main.js
+++ JavaScript/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,25 +1,35 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido')
+    }
 }
-
-// document.querySelector('.tecla_pom').onclick = tocaSomPom;
-
-
-// function tocaSomClap () {
-//     document.querySelector('#som_tecla_clap').play();
-// }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
 
-let contador = 0
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
 
-while (contador < 9) {
+    
+    tecla.onkeydown = function (evento) {
 
-    listaDeTeclas[contador].onclick = tocaSomPom;
-    contador++
+        if (evento.code == "Space" || evento.code == "Enter") {
+        tecla.classList.add('ativa');
+        }
+    }
 
-    console.log(contador);
+    tecla.onkeyup = function () {
+    tecla.classList.remove('ativa');
+    }
+
 }
